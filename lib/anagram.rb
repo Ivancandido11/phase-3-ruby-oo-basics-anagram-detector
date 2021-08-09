bbbafcfda618e306b30823ae8abb2dd10fe1b1e9
Ivan Candido <ivan.candido@powerhrg.com>
2021-08-09 08:57:58 -0400

    Completed Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array_of_words)
+    letter_array = @word.chars
+    matched_word = []
+    array_of_words.each do |word|
+      letters = word.chars
+      if letters.sort == letter_array.sort
+        matched_word << word
+      end
+    end
+    matched_word
+  end
+
+end
